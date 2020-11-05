4688ee2be4331f8b0f4557589b3992480b6c75b4
Spikoov <hem.jyh@gmail.com>
2020-11-05 15:52:56 +0100

    rm script

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("hello world");
-
-document.querySelector('drum1').addEventListener('collide', function (evt) {
-    console.log('This A-Frame entity collided with another entity!');
-});
