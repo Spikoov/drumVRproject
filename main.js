37d03ef8a3d38ef91cfdb98890ab2d73f7fa4adf
Victor BOIX <31928915+JohnSmoothie@users.noreply.github.com>
2020-11-26 13:38:15 +0100

    ajout listener

--- main.js
+++ main.js
@@ -9,3 +9,63 @@ AFRAME.registerComponent("snare", {
       })
     }
   })
+
+AFRAME.registerComponent("high-tom", {
+    init: function() {
+        this.el.addEventListener("hitstart", (e) => {
+            var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/tom-c.mp3')
+            audioCylindre1.play()
+            console.log("dans le listener")
+        })
+    }
+})
+
+AFRAME.registerComponent("mid-tom", {
+    init: function() {
+        this.el.addEventListener("hitstart", (e) => {
+            var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/tom-b.mp3')
+            audioCylindre1.play()
+            console.log("dans le listener")
+        })
+    }
+})
+
+AFRAME.registerComponent("low-tom", {
+    init: function() {
+        this.el.addEventListener("hitstart", (e) => {
+            var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/tom-a.mp3')
+            audioCylindre1.play()
+            console.log("dans le listener")
+        })
+    }
+})
+
+AFRAME.registerComponent("hi-hat", {
+    init: function() {
+        this.el.addEventListener("hitstart", (e) => {
+            var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/hi-hat.mp3')
+            audioCylindre1.play()
+            console.log("dans le listener")
+        })
+    }
+})
+
+AFRAME.registerComponent("crash", {
+    init: function() {
+        this.el.addEventListener("hitstart", (e) => {
+            var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/crash.mp3')
+            audioCylindre1.play()
+            console.log("dans le listener")
+        })
+    }
+})
+
+AFRAME.registerComponent("ride", {
+    init: function() {
+        this.el.addEventListener("hitstart", (e) => {
+            var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/ride.mp3')
+            audioCylindre1.play()
+            console.log("dans le listener")
+        })
+    }
+})
