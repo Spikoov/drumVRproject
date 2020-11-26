37091e58cbe7899c7a491c251075a08de86a8dd0
Victor BOIX <31928915+JohnSmoothie@users.noreply.github.com>
2020-11-26 11:39:01 +0100

    commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("Hello from main script")
 AFRAME.registerComponent("snare", {
     init: function() {
       this.el.addEventListener("hitstart", (e) => {
-        var audioCylindre1 = new Audio('https://raw.githubusercontent.com/Spikoov/drumVRproject/main/sound/snare.mp3')
+        var audioCylindre1 = new Audio('sound/snare.mp3')
         audioCylindre1.play()
         console.log("dans le listener")
       })
